e880b2790d08bd6a144400e41c413622f913d0a9
psnpsn <mohamed.jridi.1@esprit.tn>
2020-05-12 16:37:30 +0100

    added config-properties files, servers enabled: Eureka, Config

--- touchless-discovery/src/main/java/com/esprit/touchlessdiscovery/TouchlessDiscoveryApplication.java
+++ touchless-discovery/src/main/java/com/esprit/touchlessdiscovery/TouchlessDiscoveryApplication.java
@@ -2,7 +2,9 @@ package com.esprit.touchlessdiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class TouchlessDiscoveryApplication {
 
